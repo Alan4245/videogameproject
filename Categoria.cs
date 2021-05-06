8d9f2886296327fa68522b57f2c51b1216e82080
FrancescoTonelli <71379692+FrancescoTonelli@users.noreply.github.com>
2021-05-06 13:49:55 +0200

    Inserimento parte di Endi

---
+++ Categoria.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Esercizio_Videogioco
+{
+    public class Categoria:IEquatable<Categoria>
+    {
+        private string _id;
+        private string _nome;
+
+        public Categoria()
+        {
+            
+        }
+
+        public Categoria(string i, string n)
+        {
+            try
+            {
+                ID = i;
+                Nome = n;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Id categoria non valido");
+                _id = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Nome categoria non valido");
+                _nome = value;
+            }
+        }
+
+        public bool Equals(Categoria other)
+        {
+            if (this.ID == other.ID)
+                return true;
+            return false;
+        }
+    }
+}
---
+++ Razza.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Esercizio_Videogioco
+{
+    public class Razza:IEquatable<Razza>
+    {
+        private string _nome;
+        private List<Categoria> _categorieArmi;
+        private string _id;
+
+        public Razza()
+        {
+            
+        }
+
+        public Razza(string id, string nome, List<Categoria> cat)
+        {
+            try
+            {
+                ID = id;
+                Nome = nome;
+                CategorieArmi = cat;
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Categoria> CategorieArmi
+        {
+            get
+            {
+                return _categorieArmi;
+            }
+            set
+            {
+                _categorieArmi = value;
+            }
+        }
+
+        public string ID
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Id razza non valido");
+                _id = value;
+            }
+        }
+
+        public string Nome
+        {
+            get
+            {
+                return _nome;
+            }
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new Exception("Nome razza non valido");
+                _nome = value;
+            }
+        }
+
+        public bool Equals(Razza other)
+        {
+            if (this.ID == other.ID)
+                return true;
+            return false;
+        }
+    }
+}
